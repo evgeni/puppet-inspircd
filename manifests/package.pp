57e70506b2aaf4fe47878fc84a63022ee16d464e
Evgeni Golov <evgeni.golov@credativ.de>
2013-04-03 15:03:40 +0400

    allow installing inspircd from backports

--- manifests/package.pp
+++ manifests/package.pp
@@ -16,8 +16,20 @@
 #
 class inspircd::package (
   $ensure = hiera('ensure', $inspircd::params::ensure),
+  $use_backport = hiera('use_backport'),
 ) inherits inspircd::params {
+  if $use_backport {
+    file { '/etc/apt/preferences.d/inspircd.pref':
+      ensure  => $ensure,
+      content => template('inspircd/etc/apt/preferences.d/inspircd.pref.erb'),
+    }
+  }
   package { 'inspircd':
     ensure => $ensure,
+    require => $use_backport ? {
+      true => File['/etc/apt/preferences.d/inspircd.pref'],
+      default => [],
+    }
   }
 }
+
